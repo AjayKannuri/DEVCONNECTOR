e79ca1de8f0e512a4494b2a9c92bf1e2cd757d2a
KannuriAjay <kannuriajay5@gmail.com>
2019-05-22 02:02:35 +0530

    working on the register.js 5/22/2019

--- one.sh
+++ one.sh
@@ -1,2 +1,9 @@
 // this is ajay kannuri
 // npm i express express-validator bcryptjs config gravatar jsonwebtoken mongoose request
+
+
+
+// npm install -g create-react-app
+// npx create-react-app client
+
+// npm i axios react-router-dom redux react-redux redux-thunk redux-devtools-extension moment react-moment
